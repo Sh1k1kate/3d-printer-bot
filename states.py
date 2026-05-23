708aecfcfcf277a2ae3e197a5815a183a2098015
Sh1k1kate <100587221+Sh1k1kate@users.noreply.github.com>
2026-05-23 17:12:12 +0700

    Update states.py

--- states.py
+++ states.py
@@ -4,3 +4,7 @@ class AddModel(StatesGroup):
     waiting_for_model_name = State()
     waiting_for_details_count = State()
     waiting_for_detail = State()
+
+class EditModel(StatesGroup):
+    waiting_for_new_value = State()   # общее состояние для ввода нового значения параметра
+    # в data будем хранить: model_name, det_name, param, current_value
