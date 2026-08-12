c06a46cc1012e514058e3609fdc62f558be494a8
Sh1k1kate <100587221+Sh1k1kate@users.noreply.github.com>
2026-08-12 12:20:49 +0300

    Update states.py

--- states.py
+++ states.py
@@ -11,7 +11,19 @@ class EditModel(StatesGroup):
 class CreateOrder(StatesGroup):
     waiting_for_model = State()
     waiting_for_quantity = State()
-    waiting_for_deadline = State()   # вызов календаря
+    waiting_for_deadline = State()
 
 class EditOrder(StatesGroup):
     waiting_for_new_printed = State()
+
+class AddKit(StatesGroup):
+    waiting_for_kit_name = State()
+    waiting_for_item = State()                     # выбор модели из списка
+    waiting_for_quantity_for_item = State()        # ввод количества для выбранной модели
+    waiting_for_price = State()
+    waiting_for_description = State()
+
+class EditKit(StatesGroup):
+    waiting_for_new_value = State()                # для name, price, desc
+    waiting_for_item_edit = State()                # редактирование состава (выбор действия)
+    waiting_for_quantity_edit = State()            # ввод количества при добавлении модели
