5f4428b58d938a7eaf6adc355b11f75c1493c227
Sh1k1kate <100587221+Sh1k1kate@users.noreply.github.com>
2026-09-17 18:38:53 +0700

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,7 +1,17 @@
 from .admin import router as admin_router
+from .common import router as common_router
 from .models import router as models_router
 from .kits import router as kits_router
 from .orders import router as orders_router
 from .tasks import router as tasks_router
 
-routers = [admin_router, models_router, kits_router, orders_router, tasks_router]
+# Порядок важен: common_router должен быть в конце (у него fallback-обработчик),
+# но перед этим обработчики ignore и middleware
+routers = [
+    admin_router,
+    models_router,
+    kits_router,
+    orders_router,
+    tasks_router,
+    common_router,
+]
